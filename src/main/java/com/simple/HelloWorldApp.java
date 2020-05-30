4b2b19271695842b63364ca5182c0733f5970197
Dmitry Lekhtuz <lekhtuz@gmail.com>
2020-05-29 21:48:27 -0400

    Initial release

---
+++ src/main/java/com/simple/HelloWorldApp.java
@@ -0,0 +1,20 @@
+package com.simple;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class HelloWorldApp {
+	public static void main(String[] args)
+	{
+		SpringApplication.run(HelloWorldApp.class, args);
+	}
+
+	@GetMapping
+	public String hello() {
+		return "Hello world!";
+	}
+}
